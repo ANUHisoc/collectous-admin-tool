81275033d1edce7420eabbf37cb4b34d6f83c0f8
kalai <ssokalai@gmail.com>
2021-07-25 03:21:37 +0800

    fix: Server function copyContent creating extra top level folder

--- collectous-admin-tool/src/server/drive-utils.ts
+++ collectous-admin-tool/src/server/drive-utils.ts
@@ -3,7 +3,7 @@ export const SHEETS_MIME_TYPE = "application/vnd.google-apps.spreadsheet";
 import Folder = GoogleAppsScript.Drive.Folder;
 import File = GoogleAppsScript.Drive.File;
 
-export function getFolders(rootFolder:Folder, condition, limit = 1):Folder[] {
+export function getFolders(rootFolder: Folder, condition, limit = 1): Folder[] {
     var stack = [rootFolder]
     var result = []
     while ((stack.length !== 0) && (result.length < limit)) {
@@ -20,7 +20,7 @@ export function getFolders(rootFolder:Folder, condition, limit = 1):Folder[] {
 }
 
 
-export function getFiles(rootFolder:Folder, condition, limit = 1):File[] {
+export function getFiles(rootFolder: Folder, condition, limit = 1): File[] {
     var stack = [rootFolder]
     var result = []
     while ((stack.length !== 0) && (result.length < limit)) {
@@ -44,13 +44,21 @@ export function getFiles(rootFolder:Folder, condition, limit = 1):File[] {
 
 
 
-export function copyContent(sourceFolder:Folder, targetFolder:Folder, isSourceUnderTarget = false) {
+export function copyContent(sourceFolder, targetFolder) {
     var sourceFolderStack = [sourceFolder]
     var currentTargetFolder = targetFolder
-    var isRoot = true
+    var isRoot = true;
 
     while (sourceFolderStack.length !== 0) {
         var currentSourceFolder = sourceFolderStack.pop()
+        
+        if (!isRoot) {
+            currentTargetFolder = currentTargetFolder.createFolder(currentSourceFolder.getName())
+        }
+        else {
+            isRoot = false
+        }
+
 
         var folderIterator = currentSourceFolder.getFolders()
         while (folderIterator.hasNext()) {
@@ -58,26 +66,19 @@ export function copyContent(sourceFolder:Folder, targetFolder:Folder, isSourceUn
             sourceFolderStack.push(folder)
         }
 
-        if (isRoot) {
-            isRoot = false;
-            if (isSourceUnderTarget) {
-                currentTargetFolder = currentTargetFolder.createFolder(currentSourceFolder.getName())
-                continue;
-            }
-        }
 
         var fileIterator = currentSourceFolder.getFiles()
         while (fileIterator.hasNext()) {
             var file = fileIterator.next()
             file.makeCopy(file.getName(), currentTargetFolder)
         }
-        currentTargetFolder = currentTargetFolder.createFolder(currentSourceFolder.getName())
+
 
     };
 }
 
 
-export function isDriveFolderTreeIdentical(sourceFolder:Folder, targetFolder:Folder):boolean {
+export function isDriveFolderTreeIdentical(sourceFolder: Folder, targetFolder: Folder): boolean {
     // TODO: Will be used to check if it is system compatible folder where the sourceFolder will be the template.
     return false;
 }
@@ -87,19 +88,19 @@ export function getFolder(id: string) {
     return DriveApp.getFolderById(id)
 }
 
-export function getCurrentScriptFile():File {
+export function getCurrentScriptFile(): File {
     return DriveApp.getFileById(ScriptApp.getScriptId())
 }
 
-export function getParentFolder():Folder {
+export function getParentFolder(): Folder {
     return getCurrentScriptFile().getParents().next();
 }
 
-export function getFileUnderParentFolder(fileName:string, parentFolder:Folder):File {
+export function getFileUnderParentFolder(fileName: string, parentFolder: Folder): File {
     return parentFolder.searchFiles("title contains '" + fileName + "'").next();
 }
 
 
-export function getFolderUnderParentFolder(folderName:string, parentFolder:Folder):Folder {
+export function getFolderUnderParentFolder(folderName: string, parentFolder: Folder): Folder {
     return parentFolder.searchFolders("title contains '" + folderName + "'").next();
 }
